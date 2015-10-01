30f3dff12af35827f35b528af4d08fa422b2a41c
Mayur Dongre <mayur.dongre@algosystech.com>
2015-10-01 23:37:05 +0530

    uni webapiservice commit

---
+++ UNIWebSiteReader/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace UNIWebSiteReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtHeading = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtExperience = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtRole = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtKeySkils = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F);
+            this.textBox1.Location = new System.Drawing.Point(32, 64);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(554, 213);
+            this.textBox1.TabIndex = 173;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(32, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 31);
+            this.button1.TabIndex = 174;
+            this.button1.Text = "Split";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtHeading
+            // 
+            this.txtHeading.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHeading.Location = new System.Drawing.Point(733, 64);
+            this.txtHeading.Name = "txtHeading";
+            this.txtHeading.Size = new System.Drawing.Size(270, 26);
+            this.txtHeading.TabIndex = 175;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(668, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 176;
+            this.label1.Text = "Heading :";
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCompanyName.Location = new System.Drawing.Point(733, 110);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(270, 26);
+            this.txtCompanyName.TabIndex = 177;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(625, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 15);
+            this.label2.TabIndex = 178;
+            this.label2.Text = "Company Name :";
+            // 
+            // txtExperience
+            // 
+            this.txtExperience.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExperience.Location = new System.Drawing.Point(733, 154);
+            this.txtExperience.Name = "txtExperience";
+            this.txtExperience.Size = new System.Drawing.Size(270, 26);
+            this.txtExperience.TabIndex = 179;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(650, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 15);
+            this.label3.TabIndex = 180;
+            this.label3.Text = "Experience :";
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLocation.Location = new System.Drawing.Point(733, 200);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(270, 26);
+            this.txtLocation.TabIndex = 181;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(665, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 15);
+            this.label4.TabIndex = 182;
+            this.label4.Text = "Location :";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCity.Location = new System.Drawing.Point(733, 241);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(270, 26);
+            this.txtCity.TabIndex = 183;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(693, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 15);
+            this.label5.TabIndex = 184;
+            this.label5.Text = "City :";
+            // 
+            // txtRole
+            // 
+            this.txtRole.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRole.Location = new System.Drawing.Point(733, 285);
+            this.txtRole.Name = "txtRole";
+            this.txtRole.Size = new System.Drawing.Size(270, 26);
+            this.txtRole.TabIndex = 185;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(693, 291);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 15);
+            this.label6.TabIndex = 186;
+            this.label6.Text = "Role :";
+            // 
+            // txtKeySkils
+            // 
+            this.txtKeySkils.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKeySkils.Location = new System.Drawing.Point(733, 326);
+            this.txtKeySkils.Name = "txtKeySkils";
+            this.txtKeySkils.Size = new System.Drawing.Size(270, 26);
+            this.txtKeySkils.TabIndex = 187;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(662, 332);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 15);
+            this.label7.TabIndex = 188;
+            this.label7.Text = "Key Skils :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1202, 538);
+            this.Controls.Add(this.txtKeySkils);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtRole);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtExperience);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHeading);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtHeading;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtExperience;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtRole;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtKeySkils;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ UNIWebSiteReader/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UNIWebSiteReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                //string result = client.DownloadString(txtName.Text);
+                //// TODO: do something with the downloaded result from the remote
+                //// web site
+
+                //textBox1.Text = result;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var str = textBox1.Text;
+
+
+            var strLines = str.Split(new[] {'\n'});
+            txtHeading.Text = strLines[0].Trim();
+                //str.Substring(0, str.IndexOf(Environment.NewLine, StringComparison.Ordinal));
+
+            var secoundLineWords = strLines[1].Split(',');
+            txtCompanyName.Text = secoundLineWords[0].Trim();
+            txtExperience.Text = secoundLineWords[1].Trim();
+            txtLocation.Text = secoundLineWords[2].Trim();
+            txtCity.Text = secoundLineWords[3].Trim();
+
+            var role = strLines[2].Substring(("Role: ").Length);
+            txtRole.Text = role.Trim();
+
+            var keySkills = strLines[3].Substring(("Key Skills: ").Length);
+            txtKeySkils.Text = keySkills.Trim();
+        }
+    }
+}
